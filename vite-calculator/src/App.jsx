1c0b342d8c55bb7fe213765b7b210b7d6fe1b140
abbydoag <abby.donis.a@gmail.com>
2024-05-30 11:54:54 -0600

    Cambios logica

--- vite-calculator/src/App.jsx
+++ vite-calculator/src/App.jsx
@@ -4,173 +4,75 @@ import Button from '@components/button.jsx'
 
 const App = () => {
   const [calculatorData, setCalculatorData] = useState({
-    result: '0',
-    currentValue: '',
-    currentOperation: '',
+    maxChar: 10,
+    result: null,
+    currentValue: '0',
+    currentOperation: null,
   })
 
   const keyMap = {
-    48: { type: 'input', value: '0' }, 
-    49: { type: 'input', value: '1' },
-    50 : { type: 'input', value: '2' },
-    51 : { type: 'input', value: '3' },
-    52 : { type: 'input', value: '4' },
-    53 : { type: 'input', value: '5' },
-    54 : { type: 'input', value: '6' },
-    55 : { type: 'input', value: '7' },
-    56 : { type: 'input', value: '8' },
-    57: { type: 'input', value: '9' }, 
-    190: { type: 'input', value: '.' }, 
-  
-    111: { type: 'mate', value: '/' }, 
-    107: { type: 'mate', value: '+' }, 
-    109: { type: 'mate', value: '-' }, 
-    106: { type: 'mate', value: '*' }, 
-  
-    8: { type: 'Del' }, // borra
-    13: { type: 'result' }, // =
-    27: { type: 'C' }, //borra todo
-  };
-  
-
-  const handleNum = (value) => {
-    setCalculatorData({ ...calculatorData, currentValue: calculatorData.currentValue + value });
+    48: {type: 'input', value: '0'}, 
+    49: {type: 'input', value: '1'},
+    50 : {type: 'input', value: '2'},
+    51 : {type: 'input', value: '3'},
+    52 : {type: 'input', value: '4'},
+    53 : {type: 'input', value: '5'},
+    54 : {type: 'input', value: '6'},
+    55 : {type: 'input', value: '7'},
+    56 : {type: 'input', value: '8'},
+    57: {type: 'input', value: '9'}, 
+    190: {type: 'input', value: '.'},
+    187: {type: 'mate', value: 'sum'}, 
+    189: {type: 'mate', value: 'sub'}, 
+    221: {type: 'mate', value: 'mult'}, 
+    47: {type: 'mate', value: 'div'}, 
+    88 : {type: 'mate', value: 'exp'},
+    8: {type: 'delete', value: null}, // borra
+    13: { type: 'result', value: null},
+    27: { type: 'clear', value: 'clear'}, //borra todo
+    84: {type: 'toggle', value: 'toggle'}
   }
-  
-  const handleSum = () => {
+  const calculator = ()=>{
     const a = parseFloat(calculatorData.result, 10)
     const b = parseFloat(calculatorData.currentValue, 10)
-    const result = a + b
+    let resultValue = 0
 
-    setCalculatorData({ ...calculatorData, currentOperation: 'sum', result: result, currentValue: '' });
-    updateDisplay();
-  }
-
-  const handleSub= () => {
-    const a = parseFloat(calculatorData.result, 10)
-    const b = parseFloat(calculatorData.currentValue, 10)
-    const result = a - b
-  }
-
-  const handleMult = () => {
-    const a = parseFloat(calculatorData.result, 10)
-    const b = parseFloat(calculatorData.currentValue, 10)
-    const result = a * b
-  }
-
-  const handleDiv = () => {
-    const a = parseFloat(calculatorData.result, 10)
-    const b = parseFloat(calculatorData.currentValue, 10)
-    if(b === 0){
-      setCalculatorData({ ...calculatorData, result: 'ERROR' })
-      return
-    }
-    const result = a/b
-
-    setCalculatorData({ ...calculatorData, currentOperation: 'div', result: result.toString(), currentValue: '' })
-    updateDisplay()
-  }
-
-  const handleClear = () => {
-    setCalculatorData({ result: '0', currentValue: '', currentOperation: '' })
-  }
-
-  const handleToggle= () => {
-    const currentValue = calculatorData.currentValue
-    let newValue
-
-    if (currentValue.startsWith('-')) {
-      newValue = currentValue.slice(1) //quita -
-    } else {
-      newValue = '-' + currentValue // da +
-    }
-
-    setCalculatorData({ ...calculatorData, currentValue: newValue })
-  }
-
-  const handleEquals = () => {
-    if (calculatorData.currentOperation === 'sum') {
-      handleSum()
-    } else if (calculatorData.currentOperation === 'sub') {
-      handleSub()
-    } else if (calculatorData.currentOperation === 'mult') {
-      handleMult()
-    } else if (calculatorData.currentOperation === 'div') {
-      handleDiv()
+    switch(calculatorData.currentOperation){
+      case 'sum':
+        resultValue = a+b
+      case 'sub':
+        resultValue = a-b
+      case 'mult':
+        resultValue = a*b
+      case 'div':
+        if(b === 0){
+          resultValue = 'ERROR'
+        }else{
+          resultValue = a/b
+        }
+      case 'exp':
+        resultValue = Math.pow(a,b)
     }
   }
-
-  // Update display
-  const updateDisplay = () => {
-    
+  const handleInput=(userInput)=>{
+    if(userInput.type === 'input'){
+      setNumb(userInput.value)
+    }
+    else if(userInput.type === 'mate') {
+      setNumb(userInput.value)
+    } else if (userInput.type === 'delete') {
+      deleteNumber()
+    } else if (userInput.type === 'clear') {
+      clearAll()
+    } else if (userInput.type === 'toggle') {
+      toggleNumber()
+    } else if (userInput.type === 'result') {
+      showResult()
+    }
   }
-
-  if (currentResult.startsWith('-')) {
-    displayValue = 'ERROR' //x<0
-  if (currentResult.length > 9) {
-      displayValue = 'ERROR'; //+9
-    }
-
-  // Tecla input
-  useEffect(() => {
-    const handleKeyboardInput = (event) => {
-      const keyCode = event.keyCode
-      
-      const processInputValue = keyMap[keyCode]
-      if (processInputValue) {
-        processInput(processInputValue)
-      }
-    }
-
-    document.addEventListener('keydown', handleKeyboardInput)
-
-    return () => document.removeEventListener('keydown', handleKeyboardInput) // C
-  }, [])  
-
-  const processInput = (processInput) => {
-    if (processInput.type === 'input') {
-      handleNum(processInput.value);
-    } else if (processInput.type === 'mate') {
-      // operaciones elect
-      if (processInput.value === '+') {
-        handleSum()
-      } else if (processInput.value === '-') {
-        handleSub()
-      } else if (processInput.value === '*') {
-        handleMult()
-      } else if (processInput.value === '/') {
-        handleDiv()
-      }
-    } else if (processInput.type === 'Del') {
-    } else if (processInput.type === 'result') {
-      handleEquals()
-    } else if (processInput.type === 'C') {
-      handleClear()
-    } else if (processInput.type === 'toggle') {
-      handleToggle()
-    }
-    const processInputValue = keyMap[keyCode]
+  
+  setNumb=(newNumb)=>{
+    let currentValue = calculatorData.currentValue
   }
-
-  return (
-    <div className="container">
-      <div className="calculator">
-        <div className="calculator-display">{calculatorData.result}</div>
-        <div className="calculator-keyboard">
-          <div className="calculator-keyboard_container_numbers">
-            <Button value="7" onClick={() => processInput({ type: 'input', value: '7' })} />
-            
-            <Button value="C" onClick={handleClear} style={{ backgroundColor: 'red' }} />
-          </div>
-          <div className="calculator-keyboard_container_operators">
-            <Button value="+" onClick={handleSum} data-keycode="107" />
-            
-            <Button value="=" onClick={handleEquals} data-keycode="13" />
-          </div>
-        </div>
-      </div>
-    </div>
-  )
-}
 }
 export default App
