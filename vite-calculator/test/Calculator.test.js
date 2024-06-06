a94826baa11603a47506b0fc22e1d244098809cd
abbydoag <abby.donis.a@gmail.com>
2024-06-05 20:01:13 -0600

    Correciones

--- vite-calculator/test/Calculator.test.js
+++ vite-calculator/test/Calculator.test.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { render, fireEvent, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
 import Calculator from '../src/Calculator'
 // Test operations
 describe('operations test', () => {
